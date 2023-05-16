99f61a68810a74db4bb9e9e100b6e82d49de4ef0
Antonio Soto <antoniosotopwr@gmail.com>
2023-05-16 17:01:32 -0600

    added smooth scrolling to the nav bar links with event delegation

--- script.js
+++ script.js
@@ -37,3 +37,21 @@ document.addEventListener('keydown', function (e) {
 btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+///////////////////////////
+//Page navigation
+
+//Steps to use EVENT DELEGATION
+//1. Add event listener to common parent element
+//2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  //Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
