76bc3d7427d870e2afca25290566b1cf510dcb0f
Antonio Soto <antoniosotopwr@gmail.com>
2023-05-31 15:23:27 -0600

    added tabbed component

--- script.js
+++ script.js
@@ -55,3 +55,37 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+//Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+  //Guard clause
+  if (!clicked) return;
+  /*This could be done with 
+  // if(clicked){
+  //   clicked.classList.add('operations__tab--active');
+  // } */
+
+  //Removing the classes before adding classes
+  tabs.forEach(tab => {
+    tab.classList.remove('operations__tab--active');
+  });
+  tabsContent.forEach(content => {
+    content.classList.remove('operations__content--active');
+  });
+
+  //Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  console.log(clicked.dataset.tab);
+  
+  //Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
